1afe9b150e6c421c6c5bd65f369870f45f59ae7e
Jud Holliday <judh@google.com>
2018-06-04 15:50:58 -0700

    test file

---
+++ IdFilter.java
@@ -0,0 +1,19 @@
+package com.google.test;
+
+import java.util.Collection;
+
+/**
+ * Filters hardcoded test accounts out of a collection of IDs.
+ * The test accounts are 100, 101 and 110.
+ */
+public class IdFilter {
+  public static boolean containsTestAccount(Collection<Long> ids) {
+    return ids.contains(100) || ids.contains(101) || ids.contains(110);
+  }
+
+  public static void filter(Collection<Long> ids) {
+    ids.remove(100);
+    ids.remove(101);
+    ids.remove(110);
+  }
+}
