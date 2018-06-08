889e0a44edc453ae20f24b681078bab33c1ca8ff
Jud Holliday <judh@google.com>
2018-06-08 14:00:50 -0700

    Added comment for testing

--- IdFilter.java
+++ IdFilter.java
@@ -6,6 +6,7 @@ import java.util.Collection;
  * Filters hardcoded test accounts out of a collection of IDs.
  * The test accounts are 100, 101 and 110.
  */
+/* Hi there */
 public class IdFilter {
   public static boolean containsTestAccount(Collection<Long> ids) {
     return ids.contains(100L) || ids.contains(101) || ids.contains(110);
