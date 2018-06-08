713d7cfb000cae1b9a2b6477e84f1c17be86e5da
djschwend2 <40070138+djschwend2@users.noreply.github.com>
2018-06-08 12:46:55 -0700

    Create SomeFile.java
    added new file.

---
+++ SomeFile.java
@@ -0,0 +1,19 @@
+package com.google.devtools.grimoire.common;
+
+public class SomeFile {
+
+  private static String message = "some text";
+
+  public void SomeMethod() {
+    otherMethod(3);
+    anotherMethod(true, false);
+    System.out.println(message);
+  }
+
+  private void otherMethod(int unused) {
+  }
+
+  private boolean anotherMethod(boolean couldBe, boolean maybeNot) {
+    return couldBe && maybeNot;
+  }
+}
